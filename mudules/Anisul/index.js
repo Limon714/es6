5c0d339b067dae33c476c5319ff3548fb09534c4
Limon714 <limon4u714@gmail.com>
2022-09-10 21:11:05 +0600

    module

---
+++ mudules/Anisul/index.js
@@ -0,0 +1,6 @@
+import { text, setText, text2, quest } from "./mymodules.js";
+
+console.log(text);
+setText("Goodbye is not bye");
+console.log(text2);
+console.log(quest);
---
+++ mudules/Anisul/mymodules.js
@@ -0,0 +1,8 @@
+export let text = "Hello Modules is okay";
+export let text2 = "Hello Es6 is okay";
+let text3 = "Why do you learn JavaScript?"
+
+export function setText(txt) {
+    text2 = txt;
+}
+export {text3 as quest};
